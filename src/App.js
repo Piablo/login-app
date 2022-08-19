1d356b92f5a4e96601335206cac82f5062d555f7
Paul <paul.buys1@gmail.com>
2022-08-19 09:41:54 +0200

    git remote add origin https://github.com/Piablo/login-app.git

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import styled from 'styled-components'
+import { Routes, Route } from 'react-router-dom'
+//pages
+import Home from './pages/Home'
+import Welcome from './pages/Welcome'
+import { Counter } from './features/counter/Counter'
 
-function App() {
+const App = () => {
+
+  //SCOPE
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Routes>
+        <Route path="/" element={<Counter/>}></Route>
+        <Route path="/welcome" element={<Welcome/>}></Route>
+      </Routes>
+    </Container>
   );
 }
+
+const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  background-color: blueviolet;
+`
+
 
 export default App;
